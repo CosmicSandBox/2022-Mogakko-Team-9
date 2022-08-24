b9a615224d9b24ff1f16b754ccf3917e2127ad7d
minsoll <sol020501@hufs.ac.kr>
2022-08-24 23:03:18 +0900

    video page

---
+++ html/videopage/videoplay.js
@@ -0,0 +1,7 @@
+// var paly = document.querySelectorAll(".play");
+// paly.addEventListener("mouseover",function() {
+//   paly.src = "pause.png";
+// });
+// paly.addEventListener("moseout", function() {
+//   paly.src= "play 1.png"
+// });
